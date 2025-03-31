30ef16e6bc73573f879da2f7a4367ac5bc9e68c0
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-03-31 13:21:24 +0100

    passed the username to the Login Component and the Chat Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Chat from './components/Chat'
+import Login from './components/Login'
 
 function App () {
     const [username, setUsername] = useState<string>('')
 
     return (
         <div className='mainCvr'>
-            {!username && <Login />}
-            {username && <Chat />}
+            {!username && <Login updateUsername={setUsername} />}
+            {username && <Chat username={username} />}
         </div>
     )
 }
