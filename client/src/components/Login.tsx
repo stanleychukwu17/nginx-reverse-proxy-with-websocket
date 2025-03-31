990a47fcb55110cf77e36f418a918ab8be372ac3
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-03-31 13:22:30 +0100

    done with user login

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,14 +1,37 @@
+import { useState } from "react"
+import "./Login.css"
 
-export default function Login() {
+type LoginProps = {
+    updateUsername: (username: string) => void
+}
+
+export default function Login({updateUsername}: LoginProps) {
+    const [username, setUsername] = useState<string>('')
 
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Username" />
-            </div>
-            <div className="input_cvr">
-                <button type="submit">Login</button>
-            </div>
-        </form>
+        <div className="login_cvr">
+            <form
+                className="form_cvr"
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    updateUsername(username)
+                }}
+            >
+                <div className="title"><h2>Login</h2></div>
+                <div className="input_cvr">
+                    <input
+                        type="text"
+                        placeholder="Enter Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        minLength={4}
+                        required
+                    />
+                </div>
+                <div className="btn_cvr">
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </div>
     )
 }
