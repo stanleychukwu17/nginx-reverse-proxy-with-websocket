09816836e5857178a5d4b6b4f8f3e2eaddc7776d
Chukwu Stanley <stanleychukwu17@yahoo.com>
2025-03-31 08:58:27 +0100

    removed the old WebSocketProvider wrapper...

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css'
-import { WebSocketProvider } from './context/WebSocketContext';
 import Chat from './components/Chat'
 
 function App () {
     return (
-        <div>
-            <WebSocketProvider>
-                <Chat />
-            </WebSocketProvider>
+        <div className='mainCvr'>
+            <Chat />
         </div>
     )
 }
